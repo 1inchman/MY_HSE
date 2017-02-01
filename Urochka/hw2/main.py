edc965658af5501a18ad6df91b7bbf348b95da4e
1inchman <a.butenko.o@gmail.com>
2017-02-01 21:50:24 +0300

    Urochka hw2 update

--- Urochka/hw2/main.py
+++ Urochka/hw2/main.py
@@ -27,11 +27,13 @@ target = df['Price']
 X = df[cols]
 X_train, X_test, y_train, y_test = train_test_split(X, target, test_size=0.3, random_state=42)
 scaler = StandardScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
 baseline_model = np.mean(y_train)
 
-regularization_l = np.arange(0.1, 50, 0.5)
-regularization_r = np.arange(0.1, 230, 1)
+regularization_l = np.arange(0.1, 150, 0.1)
+regularization_r = np.arange(0.1, 300, 1)
 ridge_rss_train = []
 ridge_rss_test = []
 lasso_rss_train = []
@@ -42,19 +44,27 @@ ridge_params = []
 def get_RSS(y_true, y_pred):
 	return np.sum((y_true - y_pred)**2)
 
+best_model = np.mean(y_train)
+best_score = np.inf
 for l in regularization_l:
-    reg_lasso = Lasso(alpha=l, normalize=True)
+    reg_lasso = Lasso(alpha=l)
     reg_lasso.fit(X_train, y_train)
     lasso_rss_train.append(mean_squared_error(y_train, reg_lasso.predict(X_train)))
     lasso_rss_test.append(mean_squared_error(y_test, reg_lasso.predict(X_test)))
     lasso_params.append(reg_lasso.coef_)
+    if mean_squared_error(y_test, reg_lasso.predict(X_test)) < best_score:
+        best_score = mean_squared_error(y_test, reg_lasso.predict(X_test))
+        best_model = reg_lasso
 
 for l in regularization_r:
-    reg_ridge = Ridge(alpha=l, normalize=True)
+    reg_ridge = Ridge(alpha=l)
     reg_ridge.fit(X_train, y_train)
     ridge_rss_train.append(mean_squared_error(y_train, reg_ridge.predict(X_train)))
     ridge_rss_test.append(mean_squared_error(y_test, reg_ridge.predict(X_test)))
     ridge_params.append(reg_ridge.coef_)
+    if mean_squared_error(y_test, reg_ridge.predict(X_test)) < best_score:
+        best_score = mean_squared_error(y_test, reg_ridge.predict(X_test))
+        best_model = reg_ridge
 
 v_rss_t_base = np.repeat(baseline_model, y_train.size)
 v_rss_te_base = np.repeat(baseline_model, y_test.size)
@@ -84,8 +94,9 @@ plt.savefig('rss_lasso.png')
 plt.close()
 lasso_params = np.array(lasso_params).T
 ridge_params = np.array(ridge_params).T
-for lp, col in zip(lasso_params, cols):
-    plt.plot(regularization_l, lp, label=col)
+colors = sns.color_palette(palette='husl', n_colors=reg_ridge.coef_.size)
+for lp, col, color in zip(lasso_params, cols, colors):
+    plt.plot(regularization_l, lp, label=col, color=color)
 
 plt.xlabel('Regularization strength')
 plt.ylabel('Parameter value')
@@ -93,11 +104,16 @@ plt.legend(loc='best')
 plt.savefig('Lasso_params.png')
 
 plt.close()
-for rp, col in zip(ridge_params, cols):
-    plt.plot(regularization_r, rp, label=col)
+for rp, col, color in zip(ridge_params, cols, colors):
+    plt.plot(regularization_r, rp, label=col, color=color)
 
 plt.xlabel('Regularization strength')
 plt.ylabel('Parameter value')
 plt.legend(loc='best')
 plt.savefig('Ridge_params.png')
 
+with open('results.txt', 'w') as fout:
+    fout.write('The best model is {} \n'.format(best_model))
+    fout.write('The best score is {} \n'.format(best_score))
+    fout.write('The best params are {} \n'.format(best_model.coef_))
+print('The best model is {}, the best score is {}, the best params are {}'.format(best_model, best_score, best_model.coef_))
